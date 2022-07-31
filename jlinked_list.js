cbe7a8eb44a48c2786602da6a0e9989c27ee8ce0
Tanay Mehta <mehtatanay30@gmail.com>
2022-07-31 06:56:21 +0530

    insert function in linked list

--- jlinked_list.js
+++ jlinked_list.js
@@ -4,8 +4,26 @@ class node{
     this.next = undefined;
   }
 }
-const head = new node();
-head.data = 4;
-head.next = new node();
-head.next.data = 3;
-console.log(head.data);
+class Linked_list
+{
+  constructor(){
+    this.head = new node();
+  }
+  insert(value){
+    if (this.head.data === undefined){
+      this.head.data = value;
+      return 0;
+    }
+    var headref = this.head;
+    while(headref.next != undefined){
+      headref = headref.next;
+    }
+    headref.next = new node();
+    headref.next.data = value;
+  }
+}
+
+const ll = new Linked_list();
+ll.insert(4);
+ll.insert(5);
+console.log(ll.head.next.data);
